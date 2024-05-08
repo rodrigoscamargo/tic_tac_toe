47e53506e57e6fa4f72d34f8dd95f38a83eac045
Rodrigo Camargo <rodrigocamargo@bemol.com.br>
2024-05-07 21:35:20 -0300

    chore: initial tic tac toe game

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget{
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw const  Placeholder();
+  }
+
+}
